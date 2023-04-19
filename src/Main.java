e2859db0ee273c2e3b3fb17905b165d23290a8af
Serg <ksyssau@ya.ru>
2023-04-19 21:08:54 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -5,7 +7,7 @@ public class Main {
     public static void checkYear(int year) {
         if (year % 400 == 0) {
             System.out.println(year + " год является высокосным.");
-        } else if (year % 100 == 0 ) {
+        } else if (year % 100 == 0) {
             System.out.println(year + " год не является высокосным.");
 
         } else if (year % 4 == 0) {
@@ -14,10 +16,31 @@ public class Main {
             System.out.println(year + " год не является высокосным.");
         }
     }
+
     public static void task1() {
         checkYear(2023);
     }
-    public static void main(String[] args) {
-        task1();
+
+    public static void checkOsAndYear(int os, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (year < currentYear) {
+            System.out.print("Установите облегченную версию приложения ");
+        } else {
+            System.out.print("Установите обычную версию приложения ");
+        }
+        if (os == 0) {
+            System.out.println("для IOS по ссылке");
+        } else {
+            System.out.println("для Android по ссылке");
         }
     }
+
+    public static void task2() {
+        checkOsAndYear(1, 2022);
+    }
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+}
