1e6059a0601f4e956161e4cea0db3d0939fd0ea8
Serg <ksyssau@ya.ru>
2023-04-19 20:51:00 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void checkYear(int year) {
+        if (year % 400 == 0) {
+            System.out.println(year + " год является высокосным.");
+        } else if (year % 100 == 0 ) {
+            System.out.println(year + " год не является высокосным.");
+
+        } else if (year % 4 == 0) {
+            System.out.println(year + " год является высокосным.");
+        } else {
+            System.out.println(year + " год не является высокосным.");
+        }
+    }
+    public static void main(String[] args) {
+        checkYear(2023);
         }
     }
